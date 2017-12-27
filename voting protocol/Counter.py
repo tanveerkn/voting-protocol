fdfc22a8d06f8dfc312d1db7f9889ae02ba93143
tanveerkn <engr.tanveerkn@gmail.com>
2017-12-27 21:46:44 +0300

    Update Counter.py

--- voting protocol/Counter.py
+++ voting protocol/Counter.py
@@ -1,12 +1,17 @@
 from Storage import Storage
 from Verifier import Verifier
 import pickle
-
+from paillier_gmpy2  import *
+from Verifier import pub,priv
 
 class Counter:
     verify_server = Verifier()
     storage = Storage()
-    #global yes
+    share_common_storage=''
+    vote_count=0
+
+
+   #global yes
     #yes = 0
     #global no
     #no = 0
@@ -16,6 +21,7 @@ class Counter:
     def add_vote(self, envelop, vote, verifier):
 
         if vote == 0 or vote ==1:
+            self.vote_count=self.vote_count+1
 
             parts = envelop.split("_")
             share_common = parts[1]
@@ -23,10 +29,32 @@ class Counter:
             self.storage.write_share(share_common)
             self.storage.write_voter(vote)
             self.storage.result()
+            # self.enc_share(pub,share_common)
+            self.enc_vote(pub, vote)
             verifier.verify(vote)
-            #self.verify_server.decrypt(vote)
+            if self.vote_count==2:
+                verifier.decrypt_result(int(self.share_common_storage))
         else:
             print("Invalid vote")
+
+    def enc_vote(self,pub,vote):
+        res=e_add(pub,vote,vote)
+        self.share_common_storage=self.share_common_storage+str(res)
+        print(e_add(pub,vote,vote))
+
+    # def enc_share(self, pub, share_common,):
+    #     print(pub,share_common)
+    #     enc_share_all=e_add(pub,share_common,share_common)
+    #     print("share_add",enc_share_all)
+    #     # for char in enc_share_all:
+    #
+    #     self.share_common_storage.append(enc_share_all)
+    #     self.verify_server.decrypt_result()
+    #
+    #     print(self.share_common_storage)
+
+        # return enc_share_all
+
         #yes, no = pickle.load(open("votesCount.pickle", "rb"))
         # yes =""
         # no =""
