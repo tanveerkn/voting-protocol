fa3ba77b1f9054a6065b4b764e376b6a3f9e50a1
tanveerkn <engr.tanveerkn@gmail.com>
2017-12-27 21:43:23 +0300

    Update Storage.py

--- voting protocol/Storage.py
+++ voting protocol/Storage.py
@@ -26,7 +26,7 @@ class Storage:
         print("Yes: {0}, No: {1}").format(self.get_voter(1), self.get_voter(0))
 
         for k in self.share_counter:
-           print("{0}: {1}").format(k,self.share_counter[k])
+            print("{0}: {1}").format(k,self.share_counter[k])
 
     def write_voter(self, vote):
 
